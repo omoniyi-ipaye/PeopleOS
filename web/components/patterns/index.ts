31ef3c0581f36ba2a8f13d2c83f01fef6c0e4320
Omoniyi Ipaye <peopleperf14@gmail.com>
2026-09-06 17:12:30 +0200

    Add product pattern exports

---
+++ web/components/patterns/index.ts
@@ -0,0 +1,4 @@
+export { EvidenceQuality } from './evidence-quality'
+export type { EvidenceQualityProps } from './evidence-quality'
+export { Lifecycle } from './lifecycle'
+export type { LifecycleState, LifecycleStepProps } from './lifecycle'
